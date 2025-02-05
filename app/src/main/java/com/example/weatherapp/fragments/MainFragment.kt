0a3465a396f658f8467d65eb4a899e4d195cb2d9
= <=>
2025-02-05 15:11:51 +0500

    WA-10: 1. Created request with Volley

--- app/src/main/java/com/example/weatherapp/fragments/MainFragment.kt
+++ app/src/main/java/com/example/weatherapp/fragments/MainFragment.kt
@@ -2,6 +2,7 @@ package com.example.weatherapp.fragments
 
 import android.Manifest
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,10 +11,15 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.weatherapp.adapters.VpAdapter
 import com.example.weatherapp.databinding.FragmentMainBinding
 import com.example.weatherapp.isPermissionGranted
 import com.google.android.material.tabs.TabLayoutMediator
+const val API_KEY = "4f7f568acea84386a4c70143250302"
 
 class MainFragment : Fragment() {
 
@@ -40,6 +46,7 @@ class MainFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         checkPermission()
         init()
+        requestWeatherData("London")
     }
 
     private fun init() = with(binding) {
@@ -48,6 +55,26 @@ class MainFragment : Fragment() {
         TabLayoutMediator(tabLayout, vp){ tab, pos ->
             tab.text = tList[pos]
         }.attach()
+    }
+
+    private fun requestWeatherData(city: String) {
+        val url = "https://api.weatherapi.com/v1/forecast.json?key=" +
+                API_KEY +
+                "&q=$city" +
+                "&days=7" +
+                "&aqi=no&alerts=no"
+        val queue = Volley.newRequestQueue(context)
+        val request = StringRequest(
+            Request.Method.GET,
+            url,
+            {
+                result -> Log.d("MyFragment", "Result: $result")
+            },
+            {
+                error -> Log.d("MyFragment", "Error: $error")
+            }
+        )
+        queue.add(request)
     }
 
     private fun permissionListener() {
